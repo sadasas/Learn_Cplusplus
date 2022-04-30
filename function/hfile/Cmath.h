b121d47ecb3815d952b513f290e70dc097cffb71
wahyusrp <setio101199@gmail.com>
2022-04-30 22:55:23 +0700

    delete

--- function/hfile/Cmath.h
+++
@@ -1,6 +0,0 @@
-class Cmath
-{
-        public:
-            double Calculate(int x);
-
-};
--- function/hfile/Overloading.h
+++
@@ -1,9 +0,0 @@
-class Overloading
-{
-
-public:
-    double Result();
-    double Area (double length, double width);
-    double Area (double side);
-   
-};
