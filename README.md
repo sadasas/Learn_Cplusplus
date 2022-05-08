272498e94128e4db899a6371b38a89529bb3caeb
wahyusrp <setio101199@gmail.com>
2022-05-08 23:56:35 +0700

    add IGNORE file

---
+++ README.md
@@ -0,0 +1,4 @@
+Basic Of  C++
+
+
+tessss
---
+++ src/Main.cpp
@@ -0,0 +1,9 @@
+#include "..\hfile\Log.h"
+using namespace std;
+
+int Substract(int a, int b);
+int main()
+{
+   int a =2;
+   cout << Substract(2,1) << endl; 
+}
