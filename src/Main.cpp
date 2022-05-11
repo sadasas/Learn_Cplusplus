278d2a732df76236a90710f449838038e31b2eaa
wahyusrp <setio101199@gmail.com>
2022-05-11 19:25:57 +0700

    polymorph and interface

--- src/Main.cpp
+++ src/Main.cpp
@@ -1,14 +1,30 @@
 #include "..\hfile\Log.h"
 using namespace std;
 
-class Employe
+class Gender
+{
+public:
+    virtual string GetGender() = 0;
+};
+
+
+class Employe: public Gender
 { public:
    enum Job
    {  
       Admin, Manager, Staff
    };
-  Job job ; 
+  Job m_job ; 
 
+  virtual void PrintJob()
+  {
+      cout <<  Admin << endl;
+  }
+
+  string GetGender() override
+  {
+      return "MALE";
+  };
   ~Employe()
       {
       cout << "Destroyed" << endl;
@@ -16,27 +32,34 @@ class Employe
   
 };
 
-class Person :Employe
+class Person :public Employe
 {
 public:
-    string Name;
+    string m_name;
 
-    Person()
+    Person(std::string name = "unidentified", Employe::Job job = Employe::Staff): m_name(name)
     {
-        Name = "Unidentified";
-        job = Staff;
+        m_job = job;
     }
 
-    void Print()
+    void PrintJob () override
     {
-        cout << Name << job << endl;
+        cout << m_job << endl;
+    }
+    void PrintName()
+    {
+        cout << m_name << m_job << endl;
     }
 
 };
 int main()
 {
-   Person Wahyu;
-   Wahyu.Print();
-   
+    Person Wahyu;
+    Wahyu.PrintJob();
+
+   Employe& User = Wahyu;
+   User.PrintJob();
   
+   cout << "ssss";
+
 }
