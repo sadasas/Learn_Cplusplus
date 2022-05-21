87261d0616e8f89cbca48543970485a8fa5633ee
wahyusrp <setio101199@gmail.com>
2022-05-21 15:36:04 +0700

    Arrow operator

--- src/Main.cpp
+++ src/Main.cpp
@@ -5,36 +5,27 @@ using namespace std;
 
 class Vector2
 {
- 
+     
 public:
     int _a, _b;
 
     Vector2(int a, int b) : _a(a), _b(b) {};
 
-    Vector2 operator +(const Vector2 other) const
+    int Subtraction()
     {
-        return Vector2(_a + other._a, _b + other._b);
+        return _a + _b;
     }
+
+
 };
 int main()
 {
     {
-        weak_ptr<Vector2> pointA;
-        {
-            //automate destroy when reference count all 0 / null
-            shared_ptr<Vector2> pointB(new Vector2(1, 2));
-            //reference count increase of pointB
-            pointA = pointB;
-        }
-        // out of scope of pointB but count reference still alive
-        // not  freeing memory of pointB
-    }
-    // out of scope of pointA and the reference count to the poinB decrease
-    // the reference count  pointB are 0 
-    // automate freeing memory of pointB allocation
-    
-    
+      Vector2* pointA = new Vector2(1, 2);
+      cout <<  pointA->Subtraction();
 
+    }
+ 
     
     cout << "end";
 
