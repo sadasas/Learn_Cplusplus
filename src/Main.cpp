d672e8a2ca6ca5d631c7971071f88008e9f57b1c
wahyusrp <setio101199@gmail.com>
2022-05-31 17:22:13 +0700

    Macros

--- src/Main.cpp
+++ src/Main.cpp
@@ -5,33 +5,20 @@
 #include <vector>
 using namespace std;
 
-//template in class
-template<typename T, int A>
-class Person
-{
-    T availableSlot[A];
-public:
 
-    T GetSlot() const
-    {
-        return availableSlot;
-    }
-};
 
-//template in function
-template<typename type>
-void Print(type value)
-{
-    cout << value << endl;
-}
+#define DEBUG_MODE
+
+#ifdef DEBUG_MODE
+
+#define WRITE  cout << "end" ;
+
+#endif
+
 
 int main()
 {
-    Print(5);
-    Print("sadsa");
-
-    Person<int,4> wahyu;
-    wahyu.GetSlot();
-    cout << "end";
-
+   
+    WRITE
+    return 0;
 }
